45063a0e096cf2f627063fceffa1c395e3bfa343
DracoWhitefire <dracowhitefire@gmail.com>
2026-03-12 20:59:30 +0100

    Simpler, unified extensions

--- src/model.rs
+++ src/model.rs
@@ -13,56 +13,46 @@ mod prelude {
 #[cfg(any(feature = "alloc", feature = "std"))]
 pub use prelude::{String, Vec};
 
-#[repr(u8)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ExtensionTag {
-    Cea861 = 0x02,
-    DisplayId = 0x70,
-    Unknown(u8),
-}
-
-impl From<u8> for ExtensionTag {
-    fn from(tag: u8) -> Self {
-        match tag {
-            0x02 => ExtensionTag::Cea861,
-            0x70 => ExtensionTag::DisplayId,
-            _ => ExtensionTag::Unknown(tag),
-        }
-    }
-}
-
 pub struct ExtensionRegistry {
     #[cfg(any(feature = "alloc", feature = "std"))]
-    pub custom_tags: Vec<u8>,
+    pub known_tags: Vec<u8>,
 }
 
 impl ExtensionRegistry {
     pub fn new() -> Self {
+        #[cfg(any(feature = "alloc", feature = "std"))]
+        let mut known_tags = Vec::new();
+        #[cfg(any(feature = "alloc", feature = "std"))]
+        {
+            known_tags.push(0x02); // CEA-861
+            known_tags.push(0x70); // DisplayID
+        }
+
         Self {
             #[cfg(any(feature = "alloc", feature = "std"))]
-            custom_tags: Vec::new(),
+            known_tags,
         }
     }
 
     #[cfg(any(feature = "alloc", feature = "std"))]
     pub fn register(&mut self, tag: u8) {
-        if !self.custom_tags.contains(&tag) {
-            self.custom_tags.push(tag);
+        if !self.known_tags.contains(&tag) {
+            self.known_tags.push(tag);
         }
     }
 
     pub fn is_known(&self, tag: u8) -> bool {
-        if !matches!(ExtensionTag::from(tag), ExtensionTag::Unknown(_)) {
-            return true;
-        }
-
         #[cfg(any(feature = "alloc", feature = "std"))]
         {
-            return self.custom_tags.contains(&tag);
+            return self.known_tags.contains(&tag);
         }
 
         #[cfg(not(any(feature = "alloc", feature = "std")))]
-        false
+        {
+            // In a truly no_alloc/no_std environment, we might have to hardcode standard tags
+            // or provide a different mechanism. For now, let's keep it simple.
+            tag == 0x02 || tag == 0x70
+        }
     }
 }
 
