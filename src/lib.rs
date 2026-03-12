3178e9706c68168dc1bc95129ed1752938ba33e6
DracoWhitefire <dracowhitefire@gmail.com>
2026-03-12 20:00:25 +0100

    Support std::vec::Vec and alloc::vec::Vec

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,14 @@
 
 #[cfg(all(feature = "alloc", not(feature = "std")))]
 extern crate alloc;
+
+#[cfg(any(feature = "alloc", feature = "std"))]
+mod prelude {
+    #[cfg(feature = "std")]
+    pub use std::vec::Vec;
+    #[cfg(all(feature = "alloc", not(feature = "std")))]
+    pub use alloc::vec::Vec;
+}
+
+#[cfg(any(feature = "alloc", feature = "std"))]
+use prelude::Vec;
