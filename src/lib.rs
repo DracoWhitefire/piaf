10ca625a2e702e791d36cbf4607bbbc455c7fc06
DracoWhitefire <dracowhitefire@gmail.com>
2026-03-12 20:16:13 +0100

    Adds DisplayCapabilities struct and capabilities_from_edid signature

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,18 @@ extern crate alloc;
 
 #[cfg(any(feature = "alloc", feature = "std"))]
 mod prelude {
-    #[cfg(all(feature = "alloc", not(feature = "std")))]
-    pub use alloc::vec::Vec;
+    #[cfg(feature = "std")]
+    pub use std::string::String;
     #[cfg(feature = "std")]
     pub use std::vec::Vec;
+    #[cfg(all(feature = "alloc", not(feature = "std")))]
+    pub use alloc::string::String;
+    #[cfg(all(feature = "alloc", not(feature = "std")))]
+    pub use alloc::vec::Vec;
 }
 
 #[cfg(any(feature = "alloc", feature = "std"))]
-use prelude::Vec;
+use prelude::{String, Vec};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum EdidWarning {
@@ -38,4 +42,23 @@ pub struct ParsedEdid {
 
 pub fn parse_edid(_bytes: &[u8]) -> Result<ParsedEdid, EdidError> {
     unimplemented!("EDID parsing is not yet implemented");
+}
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct DisplayCapabilities {
+    #[cfg(any(feature = "alloc", feature = "std"))]
+    pub manufacturer: Option<String>,
+    pub product_code: Option<u16>,
+    pub serial_number: Option<u32>,
+    #[cfg(any(feature = "alloc", feature = "std"))]
+    pub display_name: Option<String>,
+    pub digital: bool,
+    pub width_cm: Option<u16>,
+    pub height_cm: Option<u16>,
+    #[cfg(any(feature = "alloc", feature = "std"))]
+    pub warnings: Vec<EdidWarning>,
+}
+
+pub fn capabilities_from_edid(_edid: &ParsedEdid) -> DisplayCapabilities {
+    unimplemented!("Capability derivation is not yet implemented")
 }
