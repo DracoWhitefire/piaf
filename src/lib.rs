51f8c6d35c23e49c92b58da49c114be24e3c3ada
DracoWhitefire <dracowhitefire@gmail.com>
2026-03-12 20:22:06 +0100

    Move structs and enums to model

--- src/lib.rs
+++ src/lib.rs
@@ -3,60 +3,12 @@
 #[cfg(all(feature = "alloc", not(feature = "std")))]
 extern crate alloc;
 
-#[cfg(any(feature = "alloc", feature = "std"))]
-mod prelude {
-    #[cfg(feature = "std")]
-    pub use std::string::String;
-    #[cfg(feature = "std")]
-    pub use std::vec::Vec;
-    #[cfg(all(feature = "alloc", not(feature = "std")))]
-    pub use alloc::string::String;
-    #[cfg(all(feature = "alloc", not(feature = "std")))]
-    pub use alloc::vec::Vec;
-}
+pub mod model;
+pub use model::{DisplayCapabilities, EdidError, EdidWarning, ParsedEdid};
 
-#[cfg(any(feature = "alloc", feature = "std"))]
-use prelude::{String, Vec};
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum EdidWarning {
-    UnknownExtension(u8),
-    DescriptorParseFailed,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum EdidError {
-    InvalidLength,
-    InvalidHeader,
-    ChecksumMismatch,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct ParsedEdid {
-    pub base_block: [u8; 128],
-    #[cfg(any(feature = "alloc", feature = "std"))]
-    pub extensions: Vec<[u8; 128]>,
-    #[cfg(any(feature = "alloc", feature = "std"))]
-    pub warnings: Vec<EdidWarning>,
-}
 
 pub fn parse_edid(_bytes: &[u8]) -> Result<ParsedEdid, EdidError> {
     unimplemented!("EDID parsing is not yet implemented");
-}
-
-#[derive(Debug, Clone, PartialEq, Default)]
-pub struct DisplayCapabilities {
-    #[cfg(any(feature = "alloc", feature = "std"))]
-    pub manufacturer: Option<String>,
-    pub product_code: Option<u16>,
-    pub serial_number: Option<u32>,
-    #[cfg(any(feature = "alloc", feature = "std"))]
-    pub display_name: Option<String>,
-    pub digital: bool,
-    pub width_cm: Option<u16>,
-    pub height_cm: Option<u16>,
-    #[cfg(any(feature = "alloc", feature = "std"))]
-    pub warnings: Vec<EdidWarning>,
 }
 
 pub fn capabilities_from_edid(_edid: &ParsedEdid) -> DisplayCapabilities {
